312215b00fc1b6601176ffa2edcad8a954e7972a
Guillermo-ENTI <guillermo.barrasa@enti.cat>
2020-11-19 16:22:32 +0100

    Ejercicio condiciones, bucles y colores

--- bucles.sh
+++ bucles.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-echo "Ejercicio de bucles"
+echo "Ejercicio de bucle"
 
-for VARIABLE in alejandropresidente `ls | head -n 3` patatafrita; do
+NUMERO_ITERACiONES=0
+
+for VARIABLE in alejandropresidente `ls | head -n 7` patatafrita; do
 	if [ -e $VARIABLE ]; then
-		echo "Asies, el archivo $VARIABLE."
+		echo -e "\e[92m Asies, el archivo $VARIABLE existe.\e[0m"
 	else
-		echo "Sorry bro, el archibo $VARIABLE no esite."
+		echo -e "\e[91m Sorry bro, el archibo $VARIABLE no esite.\e[0m"
 	fi
+	NUMERO_ITERACIONES=$((NUMERO_ITERACIONES + 1))
+done
+
+echo ""
+
+NUMERO_COLOR=101
+
+for VARIABLE in rojo verde amarillo azul magenta cyan; do
+	echo -e "\e[$NUMERO_COLOR;30m Rafa said LGBTQ+ Rigths \e[0m"
+	NUMERO_COLOR=$((NUMERO_COLOR + 1))
 done
